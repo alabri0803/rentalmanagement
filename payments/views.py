93e3d94454715dbf2739b04d70f0ef6940c45510
alabri0803 <rashid92759346@gmail.com>
2025-04-06 12:14:06 +0000

    views

--- payments/views.py
+++ payments/views.py
@@ -5,8 +5,8 @@ from .models import Payment
 
 def payment_list(request):
   payments = Payment.objects.all().order_by('-payment_date')
-  return render(request, 'payments/payment_list.html', {'payments': payments})
+  return render(request, 'payments/list.html', {'payments': payments})
 
 def payment_detail(request, pk):
   payment = get_object_or_404(Payment, pk=pk)
-  return render(request, 'payments/payment_detail.html', {'payment': payment})
+  return render(request, 'payments/detail.html', {'payment': payment})
