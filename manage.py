0e9c50f1f6ef5ca40b9f9312cb47c6356cb2e935
Rashidalabri0803 <rashid92759346@gmail.com>
2025-04-06 13:32:24 +0400

    #

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import webbrowser
 
 def main():
     """Run administrative tasks."""
@@ -15,7 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    webbrowser.open('http://localhost:8000')
+    execute_from_command_line(sys.argv + ['runserver', '0.0.0.0:8000', '--noreload'])
 
 
 if __name__ == '__main__':
