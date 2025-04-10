1a30806d445baa16fce55b7bebade2e67fa43a75
alabri0803 <rashid92759346@gmail.com>
2025-04-10 13:19:51 +0400

    se

--- rental_management/settings.py
+++ rental_management/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'users.middleware.UpdateLastSeenMiddleware',
+    #'users.middleware.UpdateLastSeenMiddleware',
 ]
 
 ROOT_URLCONF = 'rental_management.urls'
