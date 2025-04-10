8952bd61b7969b2b49347505935b43718f0feb3c
alabri0803 <rashid92759346@gmail.com>
2025-04-10 11:08:59 +0000

    forms users

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,41 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Profile
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="اسم المستخدم")
-    password = forms.CharField(label="كلمة المرور", widget=forms.PasswordInput)
-    
+    username = forms.CharField(
+        label='اسم المستخدم',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'اسم المستخدم'})
+    )
+    password = forms.CharField(
+        label='كلمة المرور',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'كلمة المرور'})
+    )
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['role', 'phone', 'address']
+        fields = ['role', 'phone', 'address', 'avatar', 'language', 'company_name']
+        widgets = {
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
+            'company_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'language': forms.Select(attrs={'class': 'form-control'}),
+            'role': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        for field_name in ['username', 'password1', 'password2']:
+            self.fields[field_name].widget.attrs['class'] = 'form-control'
